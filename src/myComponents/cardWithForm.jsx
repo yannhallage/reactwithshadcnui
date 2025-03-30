153e0aac1dfea9f9091ce8e60e2da254d2adbdfe
yannhallage <contact.devhllg@gmail.com>
2025-03-30 17:43:18 +0000

    mise a jour

--- src/myComponents/cardWithForm.jsx
+++ src/myComponents/cardWithForm.jsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Separator } from "@/components/ui/separator"
 import {
   Select,
   SelectContent,
@@ -23,17 +24,17 @@ import {
   return (
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
+        <CardTitle>Total de la facture</CardTitle>
         <CardDescription>Deploy your new project in one-click.</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
+            {/* <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Name</Label>
               <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
+            </div> */}
+            {/* <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Framework</Label>
               <Select>
                 <SelectTrigger id="framework">
@@ -46,14 +47,31 @@ import {
                   <SelectItem value="nuxt">Nuxt.js</SelectItem>
                 </SelectContent>
               </Select>
-            </div>
+            </div> */}
           </div>
         </form>
+        <div className="">
+          <span>prix ttc :</span>
+          <span className="text-lg font-bold ml-7"> 1000.00 €</span>
+        </div>
+        <div className="">
+          <span>remise :</span>
+          <span className="text-lg font-bold ml-7"> 0.00 €</span>
+        </div>
+        <div className="">
+          <span>quantité :</span>
+          <span className="text-lg font-bold ml-7">14</span>
+        </div>
+        <Separator className="my-4"/>
+        <div className="">
+          <span className="text-xl">total :</span>
+          <span className="text-lg font-bold ml-7"> 1000.00 €</span>
+        </div>
       </CardContent>
-      <CardFooter className="flex justify-between">
+      {/* <CardFooter className="flex justify-between">
         <Button variant="outline">Cancel</Button>
         <Button>Deploy</Button>
-      </CardFooter>
+      </CardFooter> */}
     </Card>
   )
 }
